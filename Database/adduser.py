1c95b31c7103eb6d0fd3378927f09386c2777f2a
Muthu Adithya Ramnarayanan <59557291+rmadith@users.noreply.github.com>
2023-03-05 00:18:22 -0600

    Debug!

--- Database/adduser.py
+++ Database/adduser.py
@@ -29,6 +29,7 @@ def createsession():
     try:
         x = mongo.createsession(data["host_id"], data["type_id"])
         testid = mongo.getUser(data["host_id"])
+        print(testid)
         y = mongo.addUserToSession(x, data["host_id"], testid["access_token"], testid["refresh_token"])
         return {"id": str(x)}
     except:
