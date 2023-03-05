249f382caf6347691272295105c6dbae1682d505
Muthu Adithya Ramnarayanan <59557291+rmadith@users.noreply.github.com>
2023-03-05 00:17:37 -0600

    Unleash full create potential

--- Database/adduser.py
+++ Database/adduser.py
@@ -27,11 +27,9 @@ def getUserSession():
 def createsession():
     data = request.json
     try:
-        print(data["host_id"])
-        print(data["type_id"])
         x = mongo.createsession(data["host_id"], data["type_id"])
-        #testid = mongo.getUser(data["host_id"])
-        #y = mongo.addUserToSession(x, data["host_id"], testid["access_token"], testid["refresh_token"])
+        testid = mongo.getUser(data["host_id"])
+        y = mongo.addUserToSession(x, data["host_id"], testid["access_token"], testid["refresh_token"])
         return {"id": str(x)}
     except:
         return {"id":400}
