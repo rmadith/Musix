9811179b25aacfddab5c2d2798d16761d402ee21
Muthu Adithya Ramnarayanan <59557291+rmadith@users.noreply.github.com>
2023-03-04 20:47:36 -0600

    Changed debug method

--- Database/adduser.py
+++ Database/adduser.py
@@ -7,10 +7,11 @@ bp = Blueprint('login', __name__, url_prefix='/')
 @bp.route('/adduser', methods=['POST'])
 def addUser():
     data = request.json
+    print (data)
+
     try:
-        print (data)
         x = mongo.addUser(data['email'], data['access_token'], data['refresh_token'])
         print(x)
         return {"id": x}
     except:
-        return 400
+        return {"id":400}
