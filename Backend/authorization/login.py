f37e8ce54c71876f3db7100972251d09165911e1
Muthu Adithya Ramnarayanan <59557291+rmadith@users.noreply.github.com>
2023-03-05 08:27:01 -0600

    idek how

--- Backend/authorization/login.py
+++ Backend/authorization/login.py
@@ -130,7 +130,7 @@ def login():
         'activeSessions': list(response_3.json().keys()),
         'topArtists': top_artists_response,
         'topTracks': top_tracks_response,
-        'streaming': response_4.json().values(),
+        'streaming': response_4.json()["streaming"],
         'access_token': access_token
     }
 
